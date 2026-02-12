0c231b839c4ef59baae25c996c734d62c4d5ee55
Kadarla-Abhiram <kadarlaabhiram18@gmail.com>
2026-02-12 21:35:12 +0530

    Initial commit - VNR VJIET AI Chatbot

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request, jsonify
+from chatbot import get_bot_response
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/chat", methods=["POST"])
+def chat():
+    user_msg = request.json.get("message")
+    reply = get_bot_response(user_msg)
+    return jsonify({"reply": reply})
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
